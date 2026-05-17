c191f0d1ac1b1d85b94f34ecaa6d53b84f057979
ryohirano <111448052+ryohirano@users.noreply.github.com>
2026-05-17 22:58:38 +0900

    mdn.02追加

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,2 +1,7 @@
 const today = new Date(2026,4,17);
-console.log(today);
+
+const year = today.getFullYear();
+const month =today.getMonth()+1;
+const date =today.getDate();
+
+console.log(year + "年" + month + "月" +date + "日");
